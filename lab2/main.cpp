40cd25f439efec14edb5a5dacc972823ee023284
carrynetik <128978693+carrynetik@users.noreply.github.com>
2025-10-01 23:19:07 +0200

    ADD: LAB2

---
+++ lab2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int r;
+    float konstanta = 13.1415;
+    std::cout << "Введите радиус: ";
+    std::cin >> r;
+    std::cout << "Тип: float" << std::endl; //выбрал float, тк число с плавающей запятой(не целое)
+    std::cout << "Размер в байтах: " << sizeof(konstanta) << std::endl;
+    float minsize = pow(2, -126);
+    float maxsize = (2 - pow(2, -23)) * pow(2, 127);
+    std::cout << "Минимальное значениe: " << minsize << std::endl;
+    std::cout << "Максимальное значение: " << maxsize << std::endl;
+    std::cout << "V = " << (4 * konstanta * pow(r, 3)) / 3 << std::endl;
+    std::cout << "S = " << 4 * konstanta * pow(r, 2) << std::endl;
+    return 0;
+}
